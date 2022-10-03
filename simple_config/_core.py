02bdb845070d72309d7409f00c373395530624f0
admin <bogdan02022002@gmail.com>
2022-10-03 19:41:10 +0300

    Created basic new core functional

--- simple_config/_core.py
+++ simple_config/_core.py
@@ -1,65 +1,124 @@
 from io import TextIOWrapper
 
 class core:
-    def __INIT__(self):
-        self.section = "[DEFAULT]"
+    def __init__(self):
+        self.sectionDeffault = "DEFAULT"
 
     def parse(self, file: TextIOWrapper):
-        line = file.readline()
-        
+        self.configDict = {}
         while True:
             line = file.readline()
+            if not self._checkCorrectNameSection(line) == None:
+                nameSection = self._checkCorrectNameSection(line)
+                self.configDict[nameSection] = {}
+            if not self._returnOption(line) == None:
+                self.configDict[nameSection][self._returnOption(line)] = self._returnParam(line)
+                    
             if not line:
                 break
+
+        return self.configDict
+    
+    def _returnOption(self, line):
+        if len(line.split()) > 2:
+            if line.split()[1] == "=":
+                option = line.split()[0]
+                option = "".join(option)
+                return option
+
+    def _returnParam(self, line):
+        if len(line.split()) > 2:
+            if line.split()[1] == "=":
+
+                param = line.split()
+                del param[0:2]
+                param = " ".join(param)
+                return param
             
+
     def _checkCorrectNameSection(self, line):
         if len(line.split()) == 1:
-            if (list(line)[0] == "[") and (list(line)[-1] == "]"):
+            if (list(line)[0] == "[") and (list(line)[-2] == "]"):
                 line = list(line)
-                line.remove("[")
-                line.remove("]")
+                del line[0]
+                del line[-2:-1]
                 line = "".join(line)
-            list(line).count("[")
+                line = line.strip('\n')
+                line = line.strip('\t')
             return line
-        else: print('duuds')
+        else: return None
+    
+    def get(self, section: str, option: str):
+        try:
+            param = self.configDict[section][option]
+        except KeyError:
+            pass
+            ## need to add exception
+        else:
+            return param
+        
+    def has_option(self, section: str, option: str):
+        try:
+            self.configDict[section][option]
+        except KeyError:
+            return False
+        return True
+    
+    def has_section(self, section: str):
+        try:
+            self.configDict[section]
+        except KeyError:
+            return False
+        return True
+    
+    def add_section(self, section: str):
+        self.configDict[section] = {}
+        return True
+    
+    def set(self, section: str = None, option: str = None, value: str = None):
+        if section == None:
+            if self.has_section(self.sectionDeffault) == False:
+                self.configDict[self.sectionDeffault] = {}
+            self.configDict[self.sectionDeffault][option] = value
+        else:
+            self.configDict[section][option] = value
+        return True
 
     def write(self, file: TextIOWrapper):
-        pass
+        for key, value in self.configDict.items():
+            file.write(f'[{key}]\n')
+            for key, Value in value.items():
+                file.write(f'{key} = {Value}\n')
+            file.write('\n')
 
-####### Tested parse alorithm
-file = open('config.ini', 'r')
-try:
-    line1 = file.readline()
-    
-finally:
-    file.close()
+        
+
+####### Tested alorithm
+
+conf = core()
+
+file = open("config.ini", 'r')
+dict1 = conf.parse(file)
+file.close()
+
+param = conf.get("CONFIGS", "set1")
+request1 = conf.has_option("CONFIGS", "set1")
+request2 = conf.has_section("CONFIGS")
+request3 = conf.add_section("CONFIGS22")
+request4 = conf.has_section("CONFIGS22")
+request5 = conf.set(option = "fake", value = "True")
+
+file = open("config.ini", 'w')
+conf.write(file)
+file.close()
+
+print(dict1)
+print(param)
+print(request1)
+print(request2)
+print(request3)
+print(request4)
 
 """
-Parse algorithm:
-    before start:
-        1. line = readline
-        2. if line == Section # Section mask: [*String*] else error
-        3. create dict {line = none}
-    start parse:
-        1. line = readline
-        2. if not line == None: else return dict 
-        3. if line == Option # Option mask: *String* = *String*. Option = Param
-        4. option = option
-        5. param = param
-        6. add to dict {Section = {option_1 = param_1, ...}, ...}
-        7. return to 1
-
-Write algorithm:
-    before start:
-        1. Check correctly dict: str = {section_1 = {option_1 = param_1, ..., , option_n = param_n}, ..., section_n = {option_1 = param_1, ..., option_n = param_n}}
-    start write:
-        1. n = 0
-        2. n ++
-        3. if not dect[n] == None: else return finish
-        4. writeline f'[{dict[n]}]'
-        5. n2 = 0
-        6. n2 ++
-        7. if not dict[n[n2]] == None: else return to 2
-        8. writeline  f'{dict[n[n2]]}'
-        9. return to 6
+Need to add exceptions, create testes and import to main _config script
 """
